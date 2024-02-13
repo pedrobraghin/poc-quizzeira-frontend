8b5b44664c52b3b2c7d2b41fd8358a6591ca5be2
Pedro Braghin <braghindev@gmail.com>
2024-02-13 13:42:18 -0300

    feat: update routes file

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+export enum Routes {
+  home = "/",
+  dashboard = "/area-logada/inicio",
+  login = "/entrar",
+  error = "/erro",
+  createFlashCard = "/area-logada/flash-cards/criar",
+  forgotPass = "/esqueci-minha-senha",
+  resetPassword = "/redefinir-senha",
+}
